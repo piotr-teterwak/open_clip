25f980de3c0a1920542de21f909bed89f2b799b1
Piotr Teterwark <piotrt@scc1.bu.edu>
2024-01-26 14:24:56 -0500

    more rebuttal

--- launch_scripts/rebuttal.sh
+++ launch_scripts/rebuttal.sh
@@ -7,8 +7,8 @@
 #SBATCH --wait-all-nodes=1
 #SBATCH --job-name=vicuna_ablate_rpo
 #SBATCH --partition learnai4p
-#SBATCH -o .onager/logs/slurm/%x_%A.o
-#SBATCH -e .onager/logs/slurm/%x_%A.e
+#SBATCH -o /fsx/youngkyun/clamp/logs/slurm/%x_%A.o
+#SBATCH -e /fsx/youngkyun/clamp/logs/slurm/%x_%A.e
 
 eval "$(/data/home/pteterwak/miniconda3/bin/conda shell.bash hook)"
 conda activate llava_retrieve_v3
@@ -23,5 +23,5 @@ export NCCL_P2P_DISABLE=1
 
 cd /data/home/pteterwak/LLaVaRetrieve/open_clip
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
-srun --cpu_bind=v --accel-bind=gn python -u  src/training/main.py --train-data '/datasets01/img2dataset/laion-400m/data_partitioned/0/{00000..01066}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/1/{00000..01067}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/2/{00000..01014}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/3/{00000..01085}.tar::/fsx/pteterwak/data/imagenet-21k_wds/train/{0..399}/{000000..000003}.tar'  --train-num-samples 10000000 --dataset-type webdataset_double_tokenizer   --batch-size 64 --zeroshot-frequency 1  --precision amp --workers 5  --dataset-resampled --model rebuttal  --gather-with-grad --epochs 6 --lr 0.0005 --wd 0.5 --warmup 1220 --eps 1e-08 --local-loss --lock-image --lock-text  --pretrained laion400m_e32   --wrap-caption-long-list --logs /fsx/pteterwak/vicuna_retrieve/logs/ --grad-checkpointing --grad-clip-norm 1.0 --distill-model ViT-L-14 --distill-pretrained datacomp_xl_s13b_b90k  --lock-text-freeze-layer-norm
+srun --cpu_bind=v --accel-bind=gn python -u  src/training/main.py --train-data '/datasets01/img2dataset/laion-400m/data_partitioned/0/{00000..01066}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/1/{00000..01067}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/2/{00000..01014}.tar::/datasets01/img2dataset/laion-400m/data_partitioned/3/{00000..01085}.tar::/fsx/pteterwak/data/imagenet-21k_wds/train/{0..399}/{000000..000003}.tar'  --train-num-samples 10000000 --dataset-type webdataset_double_tokenizer   --batch-size 64 --zeroshot-frequency 1  --precision amp --workers 5  --dataset-resampled --model rebuttal  --gather-with-grad --epochs 6 --lr 0.0005 --wd 0.5 --warmup 1220 --eps 1e-08 --local-loss --lock-image --lock-text  --pretrained laion400m_e32   --wrap-caption-long-list --logs /fsx/youngkyun/yongkyun/logs/ --grad-checkpointing --grad-clip-norm 1.0 --distill-model ViT-L-14 --distill-pretrained datacomp_xl_s13b_b90k  --lock-text-freeze-layer-norm
 
